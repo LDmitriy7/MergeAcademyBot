b81de6187ef2d07e47358c1fb00eda0c4c7979c0
levch <levchenko.d.a1998@gmail.com>
2022-06-25 22:28:41 +0300

    .

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,8 @@
 from . import handlers as on
 from . import objects as objects
-from . import requests as request
+from . import requests as bot
 from .base.my_object import MyObject
 from .context import ctx
 from .run import run
+
+__all__ = ['on', 'objects', 'bot', 'MyObject', 'ctx', 'run']
--- main.py
+++ main.py
@@ -9,20 +9,20 @@ logging.basicConfig(level=20)
 @on.command('start')
 def welcome():
     if not my_objects.User.get():
-        request.send_message(f'<b>👋 Привiт {ctx.user.first_name}, дякую за реєстрацію</b>')
+        bot.send_message(f'<b>👋 Привiт {ctx.user.first_name}, дякую за реєстрацію</b>')
         my_objects.User().save()
 
-    request.send_message('<b>Обери що тебе цікавить 👇</b>')
+    bot.send_message('<b>Обери що тебе цікавить 👇</b>')
 
 
 @on.command('test')
 def test():
-    request.send_message('')
+    bot.send_message('')
 
 
 @on.text(kb.Main.master_class)
 def master_class():
-    request.send_message('ℹ️ Зараз немає доступних майстер-класів')
+    bot.send_message('ℹ️ Зараз немає доступних майстер-класів')
 
 
 run(
