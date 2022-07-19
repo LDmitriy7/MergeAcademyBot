5ae863b02d552c5414212b18a4451633b77bf10c
levch <levchenko.d.a1998@gmail.com>
2022-07-19 16:03:20 +0300

    .

--- core/loader.py
+++ core/loader.py
@@ -10,9 +10,10 @@ env.read_envfile()
 
 BOT_TOKEN = env('BOT_TOKEN')
 DB_NAME = env('DB_NAME')
+DB_HOST = env('DB_HOST')
 
 logger = logging.getLogger('bot')
-mongoengine.connect(DB_NAME)
+mongoengine.connect(DB_NAME, host=DB_HOST)
 session = requests.Session()
 
 HANDLERS: list[Handler] = []
